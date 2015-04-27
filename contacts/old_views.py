baf23801dcb369e2c4aac553a9eb40f1ed190971
Ezugworie08 <ezugworie.ikechukwu@live.com>
2015-04-27 20:00:47 +0100

    Re-implemented my views using built-in generic views.

--- contacts/old_views.py
+++ contacts/old_views.py
@@ -146,3 +146,54 @@ class UserList(APIView):
 
 class UserDetail(APIView):
     pass
+
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from contacts.models import Contact
+from contacts.serializers import ContactSerializer
+
+
+class CreateListContact(APIView):
+
+    def get(self, request):
+        contacts = Contact.objects.filter(owner=request.user)
+        result = ContactSerializer(contacts, many=True)
+        return Response(result.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        incoming = ContactSerializer(data=request.data)
+        if not incoming.is_valid():
+            return Response(incoming.errors, status=status.HTTP_400_BAD_REQUEST)
+        new_contact = incoming.create(owner=request.user)
+        outgoing = ContactSerializer(instance=new_contact)
+        return Response(outgoing.data, status=status.HTTP_201_CREATED)
+
+
+class RetrieveUpdateDeleteContact(APIView):
+
+    def get(self, request, pk):
+        contact = Contact.objects.get(pk=pk)
+        result = ContactSerializer(instance=contact)
+        return Response(result.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        """
+        This should work for PATCH too.
+        :param request:
+        :param pk:
+        :return:
+        """
+        contact = Contact.objects.get(pk=pk)
+        incoming = ContactSerializer(data=request.data)
+        if not incoming.is_valid():
+            return Response(incoming.errors, status=status.HTTP_400_BAD_REQUEST)
+        updated_contact = incoming.update(instance=contact, validated_data=incoming.validated_data,
+                                          owner=request.user)
+        result = ContactSerializer(instance=updated_contact)
+        return Response(result.data, status=status.HTTP_200_OK)
+
+    def delete(self, request, pk):
+        contact = Contact.objects.get(pk=pk)
+        contact.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- contacts/views.py
+++ contacts/views.py
@@ -2,53 +2,36 @@ __author__ = 'Ikechukwu'
 
 # My attempt at FAT Serializers and THIN Views
 
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from contacts.models import Contact
 from contacts.serializers import ContactSerializer
+from rest_framework import generics
 
 
-class CreateListContact(APIView):
+# Views rewritten using built-in generic views
 
-    def get(self, request):
-        contacts = Contact.objects.filter(owner=request.user)
-        result = ContactSerializer(contacts, many=True)
-        return Response(result.data, status=status.HTTP_200_OK)
+class CreateListContact(generics.ListCreateAPIView):
 
-    def post(self, request):
-        incoming = ContactSerializer(data=request.data)
-        if not incoming.is_valid():
-            return Response(incoming.errors, status=status.HTTP_400_BAD_REQUEST)
-        new_contact = incoming.create(owner=request.user)
-        outgoing = ContactSerializer(instance=new_contact)
-        return Response(outgoing.data, status=status.HTTP_201_CREATED)
+    model = Contact
+    serializer_class = ContactSerializer
+
+    def get_queryset(self):
+        return self.model.objects.filter(owner=self.request.user)
+
+    def post(self, request, *args, **kwargs):
+        request.data['owner'] = request.user
+        return super(CreateListContact, self).post(self, request)
 
 
-class RetrieveUpdateDeleteContact(APIView):
+class RetrieveUpdateDeleteContact(generics.RetrieveUpdateDestroyAPIView):
 
-    def get(self, request, pk):
-        contact = Contact.objects.get(pk=pk)
-        result = ContactSerializer(instance=contact)
-        return Response(result.data, status=status.HTTP_200_OK)
+    model = Contact
+    serializer_class = ContactSerializer
 
-    def put(self, request, pk):
-        """
-        This should work for PATCH too.
-        :param request:
-        :param pk:
-        :return:
-        """
-        contact = Contact.objects.get(pk=pk)
-        incoming = ContactSerializer(data=request.data)
-        if not incoming.is_valid():
-            return Response(incoming.errors, status=status.HTTP_400_BAD_REQUEST)
-        updated_contact = incoming.update(instance=contact, validated_data=incoming.validated_data,
-                                          owner=request.user)
-        result = ContactSerializer(instance=updated_contact)
-        return Response(result.data, status=status.HTTP_200_OK)
+    def put(self, request, *args, **kwargs):
+        request.data['owner'] = request.user
+        return super(RetrieveUpdateDeleteContact, self).put(self, request, *args, **kwargs)
 
-    def delete(self, request, pk):
-        contact = Contact.objects.get(pk=pk)
-        contact.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def patch(self, request, *args, **kwargs):
+        request.data['owner'] = request.user
+        return super(RetrieveUpdateDeleteContact, self).patch(self, request, *args, **kwargs)
+
